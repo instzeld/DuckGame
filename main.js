add99ea9026e6f92cc6aa34738e908dff61d8d47
Duckworth <eltaconoob7@gmail.com>
2023-06-03 00:22:33 -0600

    All index and style

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-//board
-let board;
-let boardWidth = 500;
-let boardHeight = 500;
-let context; 
-
+const idiomaIngles = {
+    title: "Simon Says",
+    how: "How to play:",
+    text:"A pattern will appear in the squares below, you will need to remember the pattern and click the squares, every level the pattern will get more colors to click! Enjoy.",
+  };
+  
+  const idiomaEspanol = {
+    title: "Simon Dice",
+    how: "Como jugar:",
+    text:"Un patrón aparecera en los cuadros de abajo, tendras que recordar el patron y darle click a los cuadrados, cadad nivel el patron tendra mas colores los cuales darles click! Disfruta.",
+  };
+  function cambiarIdioma(idioma) {
+    Object.keys(idioma).forEach(key => {
+      const elemento = document.getElementById(key);
+      if (elemento) {
+        elemento.textContent = idioma[key];
+      }
+    });
+  }
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+    }

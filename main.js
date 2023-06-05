b600f14d283b967ee3e35b3546f0c0fb00c0dcee
Duckworth <eltaconoob7@gmail.com>
2023-06-05 11:19:13 -0600

    Hola

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const idiomaIngles = {
     title: "Simon Says",
     how: "How to play:",
     text:"A pattern will appear in the squares below, you will need to remember the pattern and click the squares, every level the pattern will get more colors to click! Enjoy.",
+    button:"Start Game"
   };
   
   const idiomaEspanol = {
     title: "Simon Dice",
     how: "Como jugar:",
     text:"Un patrón aparecera en los cuadros de abajo, tendras que recordar el patron y darle click a los cuadrados, cadad nivel el patron tendra mas colores los cuales darles click! Disfruta.",
+    button: "Iniciar juego"
   };
   function cambiarIdioma(idioma) {
     Object.keys(idioma).forEach(key => {
@@ -21,7 +23,7 @@ const idiomaIngles = {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown if the user clicks outside of it
+
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -33,4 +35,19 @@ const idiomaIngles = {
         }
       }
     }
-    }
+  }
+  var square = document.querySelectorAll('.square');
+
+var colors = ['red', 'yellow', 'green', 'blue'];
+
+function selectRandomDiv() {
+  for (var i = 0; i < square.length; i++) {
+    square[i].classList.remove('red2', 'yellow2', 'green2', 'blue2');
+  }
+  var number = Math.floor(Math.random() * square.length);
+
+  square[number].classList.add(colors[number]+"2");
+
+
+  setTimeout(selectRandomDiv, 1000);
+}
